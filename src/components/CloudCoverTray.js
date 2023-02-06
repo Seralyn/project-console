9a89c9bbbdf52927b7ba84deca101d4894bb0b0a
Seralyn <seralyn.dev@gmail.com>
2023-02-06 14:27:36 +0200

    got divisions of tray, placement of items solved

--- src/components/CloudCoverTray.js
+++ src/components/CloudCoverTray.js
@@ -3,8 +3,37 @@ import React from "react";
 
 export default function CloudCoverTray() {
     return ( 
-        <div className="component-border">
-            <h1>CloudCoverTray Component</h1>
+        <div className="component-border astro-tray">
+            <div class="row">
+                <div class="col">
+                    <div className="conditions-symbol">X</div>
+                    <div className="day-of-week">Mon</div>
+                </div>
+                <div class="col">
+                    <div className="conditions-symbol">X</div>
+                    <div className="day-of-week">Tue</div>
+                </div>
+                <div class="col">
+                    <div className="conditions-symbol">X</div>
+                    <div className="day-of-week">Wed</div>
+                </div>
+                <div class="col">
+                    <div className="conditions-symbol">?</div>
+                    <div className="day-of-week">Thu</div>
+                </div>
+                <div class="col">
+                    <div className="conditions-symbol">X</div>
+                    <div className="day-of-week">Fri</div>
+                </div>
+                <div class="col">
+                    <div className="conditions-symbol">O</div>
+                    <div className="day-of-week">Sat</div>
+                </div>
+                <div class="col-last">
+                    <div className="conditions-symbol">X</div>
+                    <div className="day-of-week">Sun</div>
+                </div>
+            </div>
         </div>
      );
 }
