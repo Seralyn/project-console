51f36a1228dd32da05b763e19f4d163d2631e4da
Seralyn <seralyn.dev@gmail.com>
2023-02-06 15:04:14 +0200

    figured out grids, layout and also spacing and sizing for themetray

--- src/components/ThemeTray.js
+++ src/components/ThemeTray.js
@@ -4,12 +4,66 @@ import React from "react";
 export default function ThemeTray() {
     return ( 
         <div className="themetray-layout">
-            <div className="themetray-item">Creamy Kelp</div>
-            <div className="themetray-item">Cyberpunk</div>
-            <div className="themetray-item">Cotton Candy</div>
-            <div className="themetray-item">Clean & Simple</div>
-            <div className="themetray-item">Midnight</div>
-            <div className="themetray-item-last">Terminal</div>
+            <div className="themetray-item">
+                <div className="theme-colors-holder">
+                    <div className="color-square">1</div>
+                    <div className="color-square">2</div>
+                    <div className="color-square">3</div>
+                    <div className="color-square">4</div>
+                </div>
+                <div>Creamy Kelp</div>
+            </div>
+
+            <div className="themetray-item">
+            <div className="theme-colors-holder">
+                    <div className="color-square">1</div>
+                    <div className="color-square">2</div>
+                    <div className="color-square">3</div>
+                    <div className="color-square">4</div>
+            </div>
+                <div>Cyberpunk</div>
+            </div>
+            
+            <div className="themetray-item">
+            <div className="theme-colors-holder">
+                    <div className="color-square">1</div>
+                    <div className="color-square">2</div>
+                    <div className="color-square">3</div>
+                    <div className="color-square">4</div>
+            </div>
+                <div>Cotton Candy</div>
+            </div>
+            
+            <div className="themetray-item">
+            <div className="theme-colors-holder">
+                    <div className="color-square">1</div>
+                    <div className="color-square">2</div>
+                    <div className="color-square">3</div>
+                    <div className="color-square">4</div>
+            </div>
+                <div>Clean & Simple</div>
+            </div>
+            
+            <div className="themetray-item">
+            <div className="theme-colors-holder">
+                    <div className="color-square">1</div>
+                    <div className="color-square">2</div>
+                    <div className="color-square">3</div>
+                    <div className="color-square">4</div>
+            </div>
+                <div>Midnight</div>
+            </div>
+            
+            <div className="themetray-item-last">
+            <div className="theme-colors-holder">
+                    <div className="color-square">1</div>
+                    <div className="color-square">2</div>
+                    <div className="color-square">3</div>
+                    <div className="color-square">4</div>
+            </div>
+                <div>Terminal</div>
+            </div>
+
         </div>
      );
 }
