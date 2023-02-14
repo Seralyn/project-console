92a3337b912436cd732202455daa248c83a50503
Seralyn <seralyn.dev@gmail.com>
2023-02-14 18:10:56 +0200

    added new components for social media icons, and imported new colored icons as SVG instead of PNG

--- src/components/SocialNotificationsTray.js
+++ src/components/SocialNotificationsTray.js
@@ -1,14 +1,14 @@
 import React from "react";
-import telegramIcon from '../images/telegram.png';
 import messengerIcon from '../images/chat.png';
 import gmailIcon from '../images/envelope.png';
-// import * as React from "react"
+import TelegramAPI from "./TelegramAPI";
+import Messenger from "./Messenger";
 
 export default function SocialNotificationsTray() {
     return ( 
         <div className="social-icons-spacing">
-            <div className="horizontal-layout-child-padding"><img className="social-icon-size-constraint" src={telegramIcon} alt="Telegram chat app Icon" /></div>
-            <div className="horizontal-layout-child-padding"><img className="social-icon-size-constraint" src={messengerIcon} alt="Facebook Messenger Icon" /></div>
+            <TelegramAPI />
+            <Messenger />
             <div className="horizontal-layout-child-padding"><img className="gmail-icon-sizing-exception" src={gmailIcon} alt="Gmail Icon" /></div>
         </div>
      );
