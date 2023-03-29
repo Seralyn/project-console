3fb5ec0a972dd8fb6c681089137d8db54e17685e
Serlayn Campbell <seralyn84@gmail.com>
2023-03-29 15:16:50 +0300

    be nice

--- src/components/GreetingBox.js
+++ src/components/GreetingBox.js
@@ -9,6 +9,7 @@ export default function GreetingBox({seralynActivity, dayEpoch}) {
     return ( 
         <div className="center-text card-colorful">
             <GreetingHeader className="yellow-text" dayEpoch= {dayEpoch} />
+            <div className="yellow-text">BE NICE TO RAI-CHAN</div>
             <WhatDoing seralynActivity = {seralynActivity}/>
             
         </div>
